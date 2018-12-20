6d5221f56c619a053c99c5594d37a928230c7e7e
Cr3ativ3 D3v3lop3r <38813220+cr3ativ3-d3v3lop3r@users.noreply.github.com>
2018-12-20 21:47:25 +0000

    Initial c++17 project.

---
+++ headers/Window.h
@@ -0,0 +1,15 @@
+//
+// Created by Callum Cooper Work on 2018-12-20.
+//
+
+#ifndef A_GAME_ENGINE_WINDOW_H
+#define A_GAME_ENGINE_WINDOW_H
+
+class Window {
+
+public:
+    Window();
+    ~Window();
+};
+
+#endif //A_GAME_ENGINE_WINDOW_H
---
+++ src/Window.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Callum Cooper Work on 2018-12-20.
+//
+
+#include "../headers/Window.h"
+
+Window::Window() {
+}
+
+Window::~Window() {
+}
